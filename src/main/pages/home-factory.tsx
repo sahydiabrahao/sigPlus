8e8f4ad941b94f21029269c3e79d0027085f85ec
sahydiabrahao <sahydi@protonmail.com>
2023-04-30 14:40:47 -0400

    feat: add footer section

--- src/main/pages/home-factory.tsx
+++ src/main/pages/home-factory.tsx
@@ -1,5 +1,6 @@
 
-import Header from '@/presentation/pages/home/header/header'
+import Footer from '@/presentation/layout/footer/footer'
+import Header from '@/presentation/layout/header/header'
 import Hero from '@/presentation/pages/home/hero/hero'
 import Shop from '@/presentation/pages/home/shop/shop'
 import React from 'react'
@@ -10,6 +11,7 @@ export const MakeHome: React.FC = () => {
       <Header />
       <Hero />
       <Shop />
+      <Footer />
     </>
   )
 }
